c7e2ff9d260bc39e2e35d55d37882b55082a7180
Alex Krotz <alexkrotz@gmail.com>
2021-11-26 12:27:58 -0600

    main

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+import sys
+from shutil import copyfile
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+    args = sys.argv[1:]
+    if not args:
+        print('Usage: python main.py inputfile')
+    inputfile = args[0]
+    with open(inputfile) as f:
+        for line in f:
+            line1 = line.replace(" ", "")
+            line1 = line1.rstrip('\n')
+            name, value = line1.split("=")
+            exec(str(line), globals())
+    copyfile(inputfile, 'inputfile.tmp')
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+    if dim == 1:
+        # run 2-D code
+        if sim == 'WP':
+            # run wavepacket dynamics
+            print('Not implemented')
+            sys.exit()
+        if sim == 'MF':
+            # run mean-field dynamics
+            print('Not implemented')
+            sys.exit()
+        if sim == 'FSSH':
+            # run FSSH dynamics
+            print('Not implemented')
+            sys.exit()
+    if dim == 1:
+        # run 1-D code
+        if sim == 'WP':
+            # run wavepacket dynamics
+            print('Not implemented')
+            sys.exit()
+        if sim == 'MF':
+            # run mean-field dynamics
+            print('Not implemented')
+            sys.exit()
+        if sim == 'FSSH':
+            # run FSSH dynamics
+            print('Not implemented')
+            sys.exit()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
