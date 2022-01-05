27fd2f13698ff2f8a9e99c086ea2cc8d47527918
Alex Krotz <alexkrotz@gmail.com>
2022-01-04 22:35:31 -0600

    fixed model 2 sh, added model 3 wp and sh

--- main.py
+++ main.py
@@ -49,15 +49,16 @@ def main():
                                 del runSim
                                 del genviz
                                 del sys.modules['wp']
-            if model==2:
+            if model == 2:
                 from input import gen_wp_input_2, write_input
                 for W in W_vals:
                     for B in B_vals:
                         for A in A_vals:
                             for alpha in alpha_vals:
                                 for p_vec in p_vec_list:
-                                    input_data, filename = gen_wp_input_2(alpha, A,B,W,p_vec[0],p_vec[1],xran,yran,pxmax,pymax)
-                                    write_input(filename,input_data)
+                                    input_data, filename = gen_wp_input_2(alpha, A, B, W, p_vec[0], p_vec[1], xran,
+                                                                          yran, pxmax, pymax)
+                                    write_input(filename, input_data)
                                     with open(filename) as f:
                                         for line in f:
                                             line1 = line.replace(" ", "")
@@ -76,6 +77,33 @@ def main():
                                     del runSim
                                     del genviz
                                     del sys.modules['wp_2']
+            if model==3:
+                from input import gen_wp_input_3, write_input
+                for W in W_vals:
+                    for B in B_vals:
+                        for A in A_vals:
+                            for alpha in alpha_vals:
+                                for p_vec in p_vec_list:
+                                    input_data, filename = gen_wp_input_3(alpha, A,B,W,p_vec[0],p_vec[1],xran,yran,pxmax,pymax)
+                                    write_input(filename,input_data)
+                                    with open(filename) as f:
+                                        for line in f:
+                                            line1 = line.replace(" ", "")
+                                            line1 = line1.rstrip('\n')
+                                            name, value = line1.split("=")
+                                            exec(str(line), globals())
+                                    copyfile(filename, 'inputfile.tmp')
+                                    from wp_2 import runSim, genviz
+                                    if not (os.path.exists(calcdir)):
+                                        os.mkdir(calcdir)
+                                        runSim()
+                                        genviz()
+                                    else:
+                                        genviz()
+                                    os.remove('inputfile.tmp')
+                                    del runSim
+                                    del genviz
+                                    del sys.modules['wp_3']
 
 
         else:
